d1b672c1ca04ce9ed924129e9ae651a55d4dd443
ishi-0409 <isaku0409@icloud.com>
2026-04-15 23:00:32 +0900

    add CORS

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,9 +1,26 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+
 import pandas as pd
 import io
 import RC
+import os
 
 app = FastAPI()
+
+FRONT_URL = os.getenv("FRONT_URL")
+
+origins = [
+    FRONT_URL
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,        
+    allow_credentials=True,       
+    allow_methods=["POST", "GET"],   
+    allow_headers=["*"],          
+)
 
 @app.post("/uploadfile")
 async def upload_file(file: UploadFile = File(...)):
--- front/app.py
+++ front/app.py
@@ -1,11 +1,13 @@
 import streamlit as st
 import requests
 import pandas as pd
+import os
 
 st.title("RC計算機能")
 st.write("csvファイル形式でアップロードされた野球の打者成績に関してRCを計算してその結果を返します")
 
 uploadfile = st.file_uploader("ファイルを選択してください", type="csv")
+API_URL = os.getenv("API_URL")
 
 if uploadfile is not None:
     file = {'file': (uploadfile.name, uploadfile.getvalue(), "text/csv")}
